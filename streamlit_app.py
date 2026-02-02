87ba5ba471acc02d6e456649fd5eed54b1cfd242
nicolasnebot-sdg <nicolas.nebot@sdggroup.com>
2026-02-02 13:49:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,6 @@ if ingredients_list:
       #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
         
-      if time_to_insert:
-          session.sql(my_insert_stmt).collect()
-          st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
