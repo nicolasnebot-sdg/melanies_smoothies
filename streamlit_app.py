594d7395cbd2cb97d14e52a485d0d1d0b6d9b314
nicolasnebot-sdg <nicolas.nebot@sdggroup.com>
2026-02-02 13:48:13 +0100

    Update streamlit_app.py
    well indexation

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,14 +34,14 @@ if ingredients_list:
 
       # st.write(ingredients_string)
         
-      my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-              values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
+  my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+          values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
     
       #st.write(my_insert_stmt)
       #st.stop()
         
       #st.write(my_insert_stmt)
-      time_to_insert = st.button('Submit Order')
+  time_to_insert = st.button('Submit Order')
         
       if time_to_insert:
           session.sql(my_insert_stmt).collect()
