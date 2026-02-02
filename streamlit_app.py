7a778a9522ca65ae068d34f9fb0ffafd594aa0ab
nicolasnebot-sdg <nicolas.nebot@sdggroup.com>
2026-02-02 13:45:33 +0100

    Update streamlit_app.py
    Added information for each fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,21 +27,22 @@ if ingredients_list:
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+      ingredients_string += fruit_chosen + ' '
+      st.subheader(fruit_chosen + ' Nutrition Information')
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-        # st.write(ingredients_string)
+      # st.write(ingredients_string)
         
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
-                values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
+      my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+              values ('""" + ingredients_string + """', '"""+name_on_order+"""')"""
     
-        #st.write(my_insert_stmt)
-        #st.stop()
+      #st.write(my_insert_stmt)
+      #st.stop()
         
-        #st.write(my_insert_stmt)
-        time_to_insert = st.button('Submit Order')
+      #st.write(my_insert_stmt)
+      time_to_insert = st.button('Submit Order')
         
-        if time_to_insert:
-            session.sql(my_insert_stmt).collect()
-            st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
+      if time_to_insert:
+          session.sql(my_insert_stmt).collect()
+          st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
